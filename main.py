5ed6a8b68bcf6773970fd5cebc70aa06e2649667
pde-bakk <pde-bakk@student.codam.nl>
2020-12-12 11:51:41 +0100

    script to add querys to panda dataframe

---
+++ main.py
@@ -0,0 +1,37 @@
+from pymongo import MongoClient
+import pandas as pd
+from bson import json_util
+
+def _connect_mongo(host, port, username, password, db):
+	""" A util for making a connection to mongo"""
+
+	if username and password:
+		mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+		conn = MongoClient(mongo_uri)
+	else:
+		conn = MongoClient(host, port)
+
+	return conn[db]
+
+
+def read_mongo(db, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+	""" Read from Mongo and Store into DataFrame"""
+
+	db = _connect_mongo(host=host, port=port, username=username, password=password, db=db)
+
+	# Make a query to the specific Database and collection
+	cursor = db[collection].find()
+	# print(len(cursor))
+	# Expand the cursor and construct the DataFrame
+	df = pd.DataFrame(list(cursor))
+
+	# Delete the _id
+	if no_id:
+		del df['_id']
+
+	return df
+
+
+dataframe = read_mongo('test', 'venmo')
+
+print(dataframe['app.name'])
